60dc292fc0e3acb1a9bd6b5f4785289a887c36e2
Suraj Gupta <8700suraj@gmail.com>
2023-08-10 11:01:13 +0530

    v.1.02

--- src/components/NewItem.jsx
+++ src/components/NewItem.jsx
@@ -6,9 +6,15 @@ export class NewItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" >
-        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success" style={{left:'90%', zIndex: '1'}}> {source}  
-            </span>
-          <img src={imageUrl} className="card-img-top" alt="/public/logonm.png" />
+          <div style={{
+            display:  'flex',
+            justifyContent: 'flex-end',
+            position: 'absolute',
+            right: '0'
+          } }>       
+            <span className="badge rounded-pill bg-success" > {source}  
+            </span> </div>
+          <img src={imageUrl} className="card-img-top" alt='logonm.png' />
           <div className="card-body">
             <h5 className="card-title">{title}... </h5>
             <p className="card-text">{description}...</p>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -34,17 +34,21 @@ export class News extends Component {
     } 
 
     async updateNews(){
+      this.props.setProgress(10);
       // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1ae76980be44473f9ca501bca103aeba&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=41875034ddda46cfb420ae35965f7282&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       this.setState({loading : true});
       let data = await fetch(url);
+      this.props.setProgress(40);
       let parsedData = await data.json()
+      this.props.setProgress(70);
       console.log(parsedData);
       this.setState({
         articles: parsedData.articles,
          totalResults:parsedData.totalResults,
         loading: false
         })
+        this.props.setProgress(100);
     }
 
    async componentDidMount() {
